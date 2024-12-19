98b19ebee8d542bfd81b72911af882feb8adcef6
Jonathan Fraser <jonathanfras@gmail.com>
2024-12-18 22:20:18 -0800

    added day19 answer

---
+++ day19.py
@@ -0,0 +1,27 @@
+ options_count = 0 
+matching = 0 
+
+with open('inputs/day19.txt') as f:
+    language = set([l.strip() for l in f.readline().strip().split(',')])
+    f.readline()
+ 
+    for l in f:
+        w = l.strip()
+        arr =[0]*(len(w)+10)
+        arr[0] = 1
+        for n in range(0,len(w)):
+            for l in language:
+                if w[n:].startswith(l):
+                    arr[n+len(l)] += arr[n]
+
+        options_count += arr[len(w)]
+        if arr[len(w)] > 0:
+            matching += 1
+
+print(matching)
+print(options_count)
+
+        
+
+
+
